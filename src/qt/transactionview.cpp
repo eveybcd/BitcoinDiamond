6c7220e7f64d71df549855b0df078f1e4c0f99e8
eveybcd <developer@btcd.io>
2017-12-24 21:35:37 +0800

    fix decimal

--- src/qt/transactionview.cpp
+++ src/qt/transactionview.cpp
@@ -108,7 +108,7 @@ TransactionView::TransactionView(const PlatformStyle *platformStyle, QWidget *pa
     } else {
         amountWidget->setFixedWidth(100);
     }
-    amountWidget->setValidator(new QDoubleValidator(0, 1e20, 8, this));
+    amountWidget->setValidator(new QDoubleValidator(0, 1e20, 7, this));
     hlayout->addWidget(amountWidget);
 
     QVBoxLayout *vlayout = new QVBoxLayout(this);
