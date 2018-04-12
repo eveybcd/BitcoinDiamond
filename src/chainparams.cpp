b254eb9cac5b9bd5e2a802aec7c767f8152c5f8c
Starry9 <kenvpn@gmail.com>
2018-04-12 12:00:01 +0800

    Add pow limit for bcd  first 72 blocks  (testnet)

--- src/chainparams.cpp
+++ src/chainparams.cpp
@@ -78,6 +78,7 @@ public:
         consensus.BCDHeight = 495867;
         consensus.BCDGenesisBlockReward = 1400 * 10000 * COIN;
         consensus.BCDLastRewardHeight = 826002;
+        consensus.BCDBeginPowLimit = uint256S("0000000000ffffffffffffffffffffffffffffffffffffffffffffffffffffff");
         consensus.powLimit = uint256S("00000000ffffffffffffffffffffffffffffffffffffffffffffffffffffffff");
         consensus.nPowTargetTimespan = 14 * 24 * 60 * 60; // two weeks
         consensus.nPowTargetSpacing = 10 * 60;
@@ -184,9 +185,10 @@ public:
         consensus.BIP34Hash = uint256S("0x0000000023b3a96d3484e5abb3755c413e7d41500f8e2a5c3f0dd01299cd8ef8");
         consensus.BIP65Height = 581885; // 00000000007f6655f22f98e72ed80d8b06dc761d5da09df0fa1dc4be4f861eb6
         consensus.BIP66Height = 330776; // 000000002104c8c45e99a8853285a3b592602a3ccde2b832481da85e9e4ba182
-        consensus.BCDHeight = 1065535;
+        consensus.BCDHeight = 1065121;
         consensus.BCDGenesisBlockReward = 140 * 10000 * COIN;
         consensus.BCDLastRewardHeight = 1531604;
+        consensus.BCDBeginPowLimit = uint256S("00000000ffffffffffffffffffffffffffffffffffffffffffffffffffffffff");
         consensus.powLimit = uint256S("00000000ffffffffffffffffffffffffffffffffffffffffffffffffffffffff");
         consensus.nPowTargetTimespan = 14 * 24 * 60 * 60; // two weeks
         consensus.nPowTargetSpacing = 10 * 60;
@@ -250,7 +252,8 @@ public:
 
         checkpointData = (CCheckpointData) {
             boost::assign::map_list_of
-            ( 546, uint256S("000000002a936ca763904c3c35fce2f3556c559c0214345d31b1bcebf76acb70")),
+            ( 546, uint256S("0x000000002a936ca763904c3c35fce2f3556c559c0214345d31b1bcebf76acb70"))
+            (1065121, uint256S("0x890f1ad9a143da58806298072ad6d66e7a6086c7e6629265c9efb649630baa1c"))
         };
 
         chainTxData = ChainTxData{
