bded41f3acf3d6a35b36e78464eed1ba52048312
kingmeng-storage <meng.wang@yimai.io>
2018-04-26 10:51:11 +0800

    Revert "insignificant fix"
    This reverts commit ef0dd6e003eeab23d3bec7a09c677c33a39e0fc5.

--- src/rpc/server.cpp
+++ src/rpc/server.cpp
@@ -522,7 +522,7 @@ std::vector<std::string> CRPCTable::listCommands() const
 
 std::string HelpExampleCli(const std::string& methodname, const std::string& args)
 {
-    return "> Bitcoindiamond-cli " + methodname + " " + args + "\n";
+    return "> bitcoin-cli " + methodname + " " + args + "\n";
 }
 
 std::string HelpExampleRpc(const std::string& methodname, const std::string& args)
